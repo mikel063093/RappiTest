da4d39bfde8d6c30f64171e4767b59e887275d74
Miguel Alegria <mikel063093@gmail.com>
2018-09-09 17:38:46 -0500

    fix string resources

--- app/src/main/java/com/mike/rappi/ui/activity/MainActivity.java
+++ app/src/main/java/com/mike/rappi/ui/activity/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.mike.rappi.R;
 
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.mike.rappi.ui.adapter.ViewPagerAdapter;
@@ -19,28 +18,28 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.mainToolbar) Toolbar toolbar;
-    @BindView(R.id.tabs) TabLayout tabLayout;
-    @BindView(R.id.viewPager) ViewPager viewPager;
+  @BindView(R.id.mainToolbar) Toolbar toolbar;
+  @BindView(R.id.tabs) TabLayout tabLayout;
+  @BindView(R.id.viewPager) ViewPager viewPager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
 
-        setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+    setSupportActionBar(toolbar);
+    Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
 
-        setupViewPager(viewPager);
-        tabLayout.setupWithViewPager(viewPager);
-    }
+    setupViewPager(viewPager);
+    tabLayout.setupWithViewPager(viewPager);
+  }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new PopularFragment(), "Popular");
-        adapter.addFragment(new TopRatedFragment(), "Top Rated");
-        adapter.addFragment(new UpcomingFragment(), "Upcoming");
-        viewPager.setAdapter(adapter);
-    }
+  private void setupViewPager(ViewPager viewPager) {
+    ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+    adapter.addFragment(new PopularFragment(), "Popular");
+    adapter.addFragment(new TopRatedFragment(), "Top Rated");
+    adapter.addFragment(new UpcomingFragment(), "Upcoming");
+    viewPager.setAdapter(adapter);
+  }
 }
